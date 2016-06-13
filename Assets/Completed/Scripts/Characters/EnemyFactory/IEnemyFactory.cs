7b7ec766a9d72d8b348947dc2c7ee22b03faf6d1
Stefan Rohleder <rohleder.stefan@gmail.com>
2016-06-13 15:25:31 +0200

    added enemyFactory
    @todo: implement

---
+++ Assets/Completed/Scripts/Characters/EnemyFactory/IEnemyFactory.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Completed
+{
+    public class IEnemyFactory : MonoBehaviour {
+
+        IEnemy
+
+
+        // Use this for initialization
+        void Start() {
+
+        }
+
+        // Update is called once per frame
+        void Update() {
+
+        }
+    }
+
+}
